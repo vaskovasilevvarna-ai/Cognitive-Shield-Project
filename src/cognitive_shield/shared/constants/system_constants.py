08e43fc207893b98eed97b5eb47b54411727b133
vaskovasilevvarna-ai <vasko.vasilev.varna@gmail.com>
2026-04-27 18:00:48 +0300

    Create system_constants.py
    [sprint-0][shared] add system constants

---
+++ src/cognitive_shield/shared/constants/system_constants.py
@@ -0,0 +1,9 @@
+"""
+System-wide constants for Sprint 0.
+"""
+
+DEFAULT_LANGUAGE = "en"
+DEFAULT_POLICY_MODE = "bounded"
+DEFAULT_DECISION_READINESS = "not_ready"
+DEFAULT_CONFIDENCE_BAND = "low"
+DEFAULT_UNCERTAINTY_MODE = "visible"
